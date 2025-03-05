c98c51fa53e96c6dbe53e9c9055cd03c46df4787
Welk-dy <fernaldysaputra@gmail.com>
2025-03-06 01:33:19 +0700

    Categories dashboard before CRUD in it

--- src/components/category/categoryValidation.ts
+++
@@ -1,8 +0,0 @@
-// validation/categoryValidation.ts
-import * as Yup from "yup";
-
-const categoryValidationSchema = Yup.object({
-  categoryName: Yup.string().required("Category name is required"),
-});
-
-export default categoryValidationSchema;
